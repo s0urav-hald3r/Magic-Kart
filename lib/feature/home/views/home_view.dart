3f7631f7e55d00bcb9a56017fd26fb4809afca0f
s0urav-hald3r <bishalder96@gmail.com>
2024-08-28 21:38:33 +0530

    FIX: add search bar in home UI

--- lib/feature/home/views/home_view.dart
+++ lib/feature/home/views/home_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:magickart/feature/home/views/widgets/product_card.dart';
+import 'package:magickart/utils/constants/colors.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
 class HomeView extends StatefulWidget {
@@ -19,10 +22,63 @@ class _HomeViewState extends State<HomeView> {
       ),
       body: Skeletonizer(
         enabled: false,
-        child: ListView.builder(
-          itemCount: 7,
-          padding: EdgeInsets.fromLTRB(12.w, 12.h, 12.w, 0),
-          itemBuilder: (index, context) => const ProductCard(),
+        child: Column(
+          children: [
+            // Search Box
+            Container(
+              height: 40.h,
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(horizontal: 12.w, vertical: 12.h),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.r),
+                  border: Border.all(width: 1, color: XColor.primaryColor)),
+              child: Row(
+                children: [
+                  Gap(12.w),
+                  const Icon(Icons.search),
+                  Gap(12.w),
+                  Expanded(
+                    child: TextFormField(
+                      // controller: controller.search,
+                      // onChanged: (value) => controller.filterMods(),
+                      style: GoogleFonts.poppins(
+                        color: XColor.white,
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 2.sp,
+                        height: 0.0,
+                      ),
+                      cursorColor: XColor.primaryColor,
+                      textAlignVertical: TextAlignVertical.bottom,
+                      cursorHeight: 10.h,
+                      cursorWidth: 1.w,
+                      decoration: InputDecoration(
+                        isCollapsed: true,
+                        hintText: 'Search products',
+                        hintStyle: GoogleFonts.poppins(
+                            fontSize: 12.sp,
+                            color: XColor.white,
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 2.sp),
+                        border: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        focusedErrorBorder: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 7,
+                padding: EdgeInsets.fromLTRB(12.w, 0, 12.w, 0),
+                itemBuilder: (index, context) => const ProductCard(),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/feature/product/views/product_view.dart
+++ lib/feature/product/views/product_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class ProductView extends StatelessWidget {
   const ProductView({super.key});
@@ -9,68 +10,71 @@ class ProductView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
+      body: Skeletonizer(
+        enabled: false,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(12.r),
+                      bottomLeft: Radius.circular(12.r),
+                    )),
+                child: ClipRRect(
                   borderRadius: BorderRadius.only(
                     bottomRight: Radius.circular(12.r),
                     bottomLeft: Radius.circular(12.r),
-                  )),
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(12.r),
-                  bottomLeft: Radius.circular(12.r),
-                ),
-                child: Image.network(
-                  'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
-                  width: double.infinity,
-                  height: 400.h,
-                  fit: BoxFit.contain,
+                  ),
+                  child: Image.network(
+                    'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+                    width: double.infinity,
+                    height: 400.h,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 12.h),
-              child: Column(
-                children: [
-                  Text(
-                    'Fjallraven - Foldsack No. 1 aghhg Backpack, Fits 15 Laptops',
-                    maxLines: 2,
-                    style: GoogleFonts.poppins(fontSize: 18.sp),
-                  ),
-                  Gap(12.h),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '₹139.30',
-                        style: GoogleFonts.poppins(fontSize: 16.sp),
-                      ),
-                      ElevatedButton.icon(
-                        icon: Icon(
-                          Icons.favorite,
-                          size: 16.sp,
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 12.h),
+                child: Column(
+                  children: [
+                    Text(
+                      'Fjallraven - Foldsack No. 1 aghhg Backpack, Fits 15 Laptops',
+                      maxLines: 2,
+                      style: GoogleFonts.poppins(fontSize: 18.sp),
+                    ),
+                    Gap(12.h),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '₹139.30',
+                          style: GoogleFonts.poppins(fontSize: 16.sp),
                         ),
-                        onPressed: () {},
-                        label: Text(
-                          'Add to wishlist',
-                          style: GoogleFonts.poppins(fontSize: 12.sp),
-                        ),
-                      )
-                    ],
-                  ),
-                  Gap(12.h),
-                  Text(
-                    'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
-                    style: GoogleFonts.poppins(fontSize: 12.sp),
-                  ),
-                ],
-              ),
-            )
-          ],
+                        ElevatedButton.icon(
+                          icon: Icon(
+                            Icons.favorite,
+                            size: 16.sp,
+                          ),
+                          onPressed: () {},
+                          label: Text(
+                            'Add to wishlist',
+                            style: GoogleFonts.poppins(fontSize: 12.sp),
+                          ),
+                        )
+                      ],
+                    ),
+                    Gap(12.h),
+                    Text(
+                      'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
+                      style: GoogleFonts.poppins(fontSize: 12.sp),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
